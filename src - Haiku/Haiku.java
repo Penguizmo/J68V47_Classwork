7bcf05ab21e988ecb7b919f9b81b8d3881c07726
Penguizmo <darren_douglas82@hotmail.com>
2023-09-05 20:06:31 +0100

    4th attempt at adding the code from home

---
+++ src - Haiku/Haiku.java
@@ -0,0 +1,12 @@
+/**
+ * AI generated Haiku
+ * @author Darren Douglas
+ * @version 3
+ */
+public class Haiku {
+    public static void main (String[]args) {
+        System.out.println("Rain at Burning Man");
+        System.out.println("Trapped in mud, exodus starts");
+        System.out.println("Hello World");
+    }
+}
