a41f0d3dde138128a8be90dab42e32dc3d64e12b
darren <10080489@nescol.ac.uk>
2023-09-06 12:24:51 +0100

    Lesson 2

---
+++ src/Lesson2Input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lesson2Input {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        String firstName = "Bob";
+        System.out.println(firstName);
+        firstName = "Mary";
+        System.out.println(firstName);
+
+    }
+}
---
+++ src/Lesson2ProgrammingExercise2.java
@@ -0,0 +1,16 @@
+
+import java.net.SocketOption;
+import java.util.*;
+public class Lesson2ProgrammingExercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the first number? ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("what is the second number? ");
+        int num = scanner.next Int
+
+        System.out.
+    }
+}
