815947fc3143e5409f988b922aafcbaf83ea249e
Penguizmo <darren_douglas82@hotmail.com>
2023-09-07 19:35:02 +0100

    4th attempt at adding the code from home

--- src/Lesson2ProgrammingExercise2.java
+++ src/Lesson2ProgrammingExercise2.java
@@ -1,16 +1,20 @@
-
-import java.net.SocketOption;
 import java.util.*;
+
 public class Lesson2ProgrammingExercise2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What is the first number? ");
-        int num1 = scanner.nextInt();
+        System.out.print("What is the first number? ");
+        int num1 = input.nextInt();
 
-        System.out.print("what is the second number? ");
-        int num = scanner.next Int
-
-        System.out.
+        System.out.print("What is the second number? ");
+        int num2 = input.nextInt();
+        // Added "Total " after first ( so it shows at the start and what the numbers mean
+        System.out.println("Total " + (num1 + num2));
+        System.out.println("Total " + (num1 - num2));
+        System.out.println("Total " + (num1 * num2));
+        System.out.println("Total " + (num1 / num2));
+        System.out.println("Total " + (num1 % num2));
+        System.out.println("Total " + Math.pow (num1, num2));
     }
 }
