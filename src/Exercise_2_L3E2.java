808f9815e95a1cb6ca841ce21c08233c7f45f243
Penguizmo <darren_douglas82@hotmail.com>
2023-09-26 18:39:49 +0100

    Updated L3E2 with a few modifications and code changes

--- src/Exercise_2_L3E2.java
+++ src/Exercise_2_L3E2.java
@@ -5,9 +5,12 @@ public class Exercise_2_L3E2 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("What is the capital of Spain? ");
-        String Spain = input.nextLine();
-        System.out.println("Madrid ".toLowerCase());
-        System.out.println(Spain.contains("Madrid"));
+        String Answer1 = input.nextLine();
+        System.out.println(Answer1.toLowerCase().contains("madrid" ));
+
+        // I changed lines 8 and 9 to match your code
+        // I left my original code in lines 13 - 21 so that you can see my work.
+        // Both gave the same results
 
         System.out.println("What is the capital of England? ");
         String England = input.nextLine();
