88d50c7adc9d0b6612ecbcf1674cca61930852e4
Penguizmo <darren_douglas82@hotmail.com>
2023-09-26 16:22:20 +0100

    Updated L3E1 with a few modifications and code changes

--- src/Exercise_1_L3E1.java
+++ src/Exercise_1_L3E1.java
@@ -16,10 +16,8 @@ public class Exercise_1_L3E1 {
 
         // Use the next 2 section of code to generate a DOB and Username
 
-        System.out.println("Username: ");
-        System.out.println(name.substring(0, 1) + surname.substring(0, 3));
+        System.out.println("Username: " + name.substring(0, 1) + surname.substring(0, 3));
 
-        System.out.println("Password: ");
-        System.out.println(surname.substring(0, 1) + name.substring(0, 3).toUpperCase() + dob);
+        System.out.println("Password: " +surname.substring(0, 1) + name.substring(0, 3).toUpperCase() + dob);
     }
 }
