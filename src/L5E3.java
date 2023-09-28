e90bae143350a766a781a8d27d508a043c4eb060
Penguizmo <darren_douglas82@hotmail.com>
2023-09-28 23:26:33 +0100

    L5E3 Not Complete

--- src/L5E3.java
+++ src/L5E3.java
@@ -40,31 +40,5 @@ public class L5E3 {
         System.out.println("What is 1 + 10? ");
         String question10 = question.nextLine();
 
-
-
         // Start a score int as 0
         int score = 0;
-
-
-
-        // Loop through the questions
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println("Question " + (i+1) + ": " + questions[i]);
-            System.out.print("Enter your answer: ");
-
-            // Read user input
-            int userAnswer = scanner.nextInt();
-
-            // Check if the answer is correct
-            if (userAnswer == answers[i]) {
-                System.out.println("Correct!");
-                score++;
-            } else {
-                System.out.println("Incorrect!");
-            }
-        }
-
-        // Display final score
-        System.out.println("Quiz completed. Your score is: " + score + "/" + questions.length);
-    }
-}
