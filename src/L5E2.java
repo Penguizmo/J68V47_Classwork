c7cfa53ba84104076a3a77c9f1b7ecfa6f92b730
Penguizmo <darren_douglas82@hotmail.com>
2023-09-28 21:01:31 +0100

    L5E2 Complete

---
+++ src/L5E2.java
@@ -0,0 +1,12 @@
+public class L5E2 {
+    public static void main(String[] args) {
+            //No need for scanner as there is no user input required
+            //Reverse the code from slide 13 as sum is going negative
+        for (int count = 10; count >= 1; count--) {
+            //add %n to starte a new line
+            System.out.format("%d %n", count);
+        }
+//use } to end the for loop and have Blastoff at the end of the loop
+        System.out.print("Blastoff ");
+    }
+}
