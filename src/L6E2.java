49f338012c90bbbbce2e3fe82cfab49c32c66f81
darren <10080489@nescol.ac.uk>
2023-10-04 11:16:38 +0100

    Commit new exercise L6E2

---
+++ src/L6E2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class L6E2 {
+    public static void main(String[] args) {
+
+
+        int option = input.nextInt();
+        System.out.println("Option Selected: " = option);
+
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter an option (1 - 4): ");
+
+            if (option == 1); {
+                System.out.println("Option 1");
+
+            }else if (option == 2) {
+                System.out.println("Option 3");
+
+            }else {
+                System.out.println("Option 3");
+            }
+
+        } while (option ! = 4); {
+    }
+}
