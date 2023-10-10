ee1e69485eb0a89e705fd58e9d2299281a4a61b7
Penguizmo <darren_douglas82@hotmail.com>
2023-10-10 15:11:53 +0100

    L6E2 10/10/23

--- src/L6E2.java
+++ src/L6E2.java
@@ -2,25 +2,34 @@ import java.util.Scanner;
 
 public class L6E2 {
     public static void main(String[] args) {
-
-
-        int option = input.nextInt();
-        System.out.println("Option Selected: " = option);
-
+        Scanner input = new Scanner(System.in);
+        int option;
+        //creat all Menu options using println
         do {
-            Scanner input = new Scanner(System.in);
+            System.out.println("Menu:");
+            System.out.println("1. Say 'hello'");
+            System.out.println("2. Tell me the time");
+            System.out.println("3. Tell me a joke");
+            System.out.println("4. Quit");
             System.out.println("Enter an option (1 - 4): ");
+        //create an input  using int options
+            option = input.nextInt();
+            System.out.println("Option Selected: " + option);
 
-            if (option == 1); {
-                System.out.println("Option 1");
-
-            }else if (option == 2) {
-                System.out.println("Option 3");
-
-            }else {
-                System.out.println("Option 3");
+            if (option == 1) {
+                System.out.println("Hello");
+            } else if (option == 2) {
+        // found a java date class on google to display actual time
+                System.out.println("The current time is: " + new java.util.Date());
+            } else if (option == 3) {
+                System.out.println("Why don't scientists trust atoms?");
+                System.out.println("Because they make up everything!");
+            } else if (option != 4) {
+                System.out.println("Invalid option");
             }
 
-        } while (option ! = 4); {
+        } while (option != 4);
+
+        System.out.println("Goodbye!");
     }
 }
