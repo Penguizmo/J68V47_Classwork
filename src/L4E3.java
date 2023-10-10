87077c31dfcf1e7e1bb2246618a2cf17f5717b60
Penguizmo <darren_douglas82@hotmail.com>
2023-10-10 14:42:07 +0100

    L4e3 edits 10/10/23

--- src/L4E3.java
+++ src/L4E3.java
@@ -3,12 +3,12 @@ public class L4E3 {
     public static void main(String[] args) {
         Scanner amount = new Scanner(System.in);
 
-    System.out.print("Enter your total purchase amount: ");
-    int deposit = amount.nextInt();
+    System.out.print("Enter your total purchase amount: £");
+    double deposit = amount.nextInt();
         if (deposit >= 50) {
             System.out.println("Free Shipping!");
         } else {
-            System.out.println("Your final total is " + (deposit + 10));
+            System.out.println("Your final total is : £" + (deposit + 10));
         }
     }
 }
