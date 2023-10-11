a7c2731db2aa631b768a1bfb581083fbf676df5c
darren <10080489@nescol.ac.uk>
2023-10-11 12:07:12 +0100

    Commit new exercise L7E2

---
+++ src/L7E2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class L7E2 {
+
+    public static int calculateSum(int num1, int num2){
+        int result = num1 + num2;
+        return result;
+    }
+
+    public static int getValidNumber(String message){
+        Scanner input = new Scanner(System.in);
+        System.out.print(message);
+        int number = input.nextInt();
+        while (number < 0 || number > 10){
+            System.out.print("ERROR. Invalid number.");
+            System.out.print(message);
+            number = input.nextInt();
+        }
+        return number;
+    }
+    public static void main(String[] args) {
+        int num1 = getValidNumber("What is the first number? ");
+        int num2 = getValidNumber("What is the second number? ");
+
+        // Added "Total " after first ( so it shows at the start and what the numbers mean
+        System.out.format("The sum of %d and %d is %d", num1, num2, calculateSum(num1, num2));
+    }
+}
