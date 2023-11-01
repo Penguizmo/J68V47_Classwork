383138d3ea87000e25ec8e6b94a0b5031b431812
darren <10080489@nescol.ac.uk>
2023-11-01 12:03:14 +0000

    edits 01/11/23

---
+++ src/L8E1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class L8E1 {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Christopher", "Susan", "Bill", "Satya", "Darren"};
+        System.out.println(names[0]);
+        System.out.println(names[names.length-1] );
+        System.out.print("The number of items in the arreyis: %d", names.length);
+    }
+}
--- src/Lesson2ProgrammingExercise2.java
+++ src/Lesson2ProgrammingExercise2.java
@@ -1,20 +1,3 @@
 import java.util.*;
 
-public class Lesson2ProgrammingExercise2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("What is the first number? ");
-        int num1 = input.nextInt();
-
-        System.out.print("What is the second number? ");
-        int num2 = input.nextInt();
-        // Added "Total " after first ( so it shows at the start and what the numbers mean
-        System.out.println("Total " + (num1 + num2));
-        System.out.println("Total " + (num1 - num2));
-        System.out.println("Total " + (num1 * num2));
-        System.out.println("Total " + (num1 / num2));
-        System.out.println("Total " + (num1 % num2));
-        System.out.println("Total " + Math.pow(num1, num2));
-    }
-}
+//public class Lesson2ProgrammingExercise2 {
