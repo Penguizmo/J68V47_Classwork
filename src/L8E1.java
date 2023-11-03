e848d99f1bedb881f6e846402993eadd9300df6d
Penguizmo <darren_douglas82@hotmail.com>
2023-11-03 14:22:21 +0000

    L8E1 revised

--- src/L8E1.java
+++ src/L8E1.java
@@ -6,7 +6,7 @@ public class L8E1 {
 
         String[] names = {"Christopher", "Susan", "Bill", "Satya", "Darren"};
         System.out.println("The first Students name is " + names[0]);
-        System.out.println("The last Students name is " + names[4]);
+        System.out.println("The last Students name is " + names[names.length-1]);
         System.out.printf("The number of items in the array is: %d", names.length);
     }
 }
