fe506e88598658466c838c861beea5e365f83700
Penguizmo <darren_douglas82@hotmail.com>
2023-11-03 15:42:16 +0000

    L8E3

---
+++ src/L8E3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class L8E3 {
+    public static void main(String[] args) {
+        // Set up the directions array
+        String[] directions = {"Start by exiting the building you are currently in and turning left onto the main road.",
+                "Walk down the road for two blocks until you reach the traffic lights.",
+                "At the traffic lights, turn right onto Oak Street.",
+                "Follow Oak Street for one block then turn left onto Elm Street.",
+                "After turning onto Elm Street, walk past the grocery store on your right.",
+                "Walk to the end of Elm Street. Your doctor's office will be the third building on the right side of the road.",
+                "Enter through the front door of the office. Let the receptionist know you have arrived for your appointment.",
+
+        };
+
+        // Print out each direction
+        int step = 1;
+        for(String direction : directions) {
+            System.out.format("%d. %s %n",step,direction);
+            step ++;
+        }
+    }
+
+}
