f67594bef9a04e7083334a1809c79de6904c497e
darren <darren_douglas82@hotmail.com>
2023-12-04 11:59:48 +0000

    L11E2

---
+++ src/L11E2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class L11E2 {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("times_table.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            do {
+                line = bufferedReader.readLine();
+                if (line != null) {
+                    System.out.println(line);
+                }
+            } while (line != null);
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
